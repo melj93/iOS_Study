8d4fd89d6d5b25b5bac3295bd31e21051adf398c
Mel <3mel.dev@gmail.com>
2021-07-27 15:06:48 +0900

    AutoLayout, UI object connect
    AutoLayout - setting done
    UI object connect with ViewController

--- myClima/myClima/ViewController.swift
+++ myClima/myClima/ViewController.swift
@@ -8,10 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var weatherImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityNameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
