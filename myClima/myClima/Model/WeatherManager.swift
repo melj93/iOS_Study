bd8c3de2cded0fc314e6f1e49e4e32e622832bd5
Mel <3mel.dev@gmail.com>
2021-08-03 13:53:29 +0900

    WeatherModel add

--- myClima/myClima/Model/WeatherManager.swift
+++ myClima/myClima/Model/WeatherManager.swift
@@ -15,7 +15,7 @@ struct WeatherManager {
         print(urlString)
         
         performRequest(urlString: urlString)
-    } // 마법 주문을 외우기
+    }
     
     func performRequest(urlString: String) {
         if let url = URL(string: urlString) {
@@ -31,41 +31,22 @@ struct WeatherManager {
             }
             task.resume()
         }
-    }// 자연의 기를 받기
+    }
     
     func parseJSON(weatherData: Data){
         let decoder = JSONDecoder()
         do{
             let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
-            print(decodedData.name)
-            print(decodedData.main.temp)
-            print(decodedData.weather[0].description)
-            print(decodedData.weather[0].id)
-            
             let id = decodedData.weather[0].id
-            print(getConditionId(weatherId: id))
+            let name = decodedData.name
+            let temp = decodedData.main.temp
             
+            let weather = WeatherModel(cityName: name, temperature: temp, weatherId: id)
+            
+            print(weather)
+            let condition = weather.getConditionId(weatherId: id)
         } catch{
             print(error)
-        }
-    }// 기를 자신의 것으로 만들기
-    
-    func getConditionId(weatherId: Int) -> String {
-        switch weatherId {
-        case 200 ... 299:
-            return "cloud.bolt"
-        case 300 ... 399:
-            return "cloud.drizzle"
-        case 500 ... 599:
-            return "cloud.rain"
-        case 600 ... 699:
-            return "cloud.snow"
-        case 700 ... 799:
-            return "cloud.fog"
-        case 800 ... 804:
-            return "cloud"
-        default:
-            return "sun.max"
         }
     }
 }
---
+++ myClima/myClima/Model/WeatherModel.swift
@@ -0,0 +1,33 @@
+//
+//  WeatherModel.swift
+//  myClima
+//
+//  Created by samuel Jeong on 2021/08/03.
+//
+
+import Foundation
+
+struct WeatherModel {
+    let cityName: String
+    let temperature: Double
+    let weatherId: Int
+    
+    var conditionName: String {
+        switch weatherId {
+        case 200 ... 299:
+            return "cloud.bolt"
+        case 300 ... 399:
+            return "cloud.drizzle"
+        case 500 ... 599:
+            return "cloud.rain"
+        case 600 ... 699:
+            return "cloud.snow"
+        case 700 ... 799:
+            return "cloud.fog"
+        case 800 ... 804:
+            return "cloud"
+        default:
+            return "sun.max"
+        }
+    }
+}
