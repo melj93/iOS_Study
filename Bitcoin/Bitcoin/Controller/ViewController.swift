a81f5a31f0d884a63dac12c6ea0788b8fb9b5652
Mel <3mel.dev@gmail.com>
2021-08-10 13:13:44 +0900

    UIPickerViewDataSource 설정
    코너매니저.화폐배열.카운트 로 Row설정.

--- Bitcoin/Bitcoin/Controller/ViewController.swift
+++ Bitcoin/Bitcoin/Controller/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    @IBOutlet weak var currencyLabel: UILabel!
+    @IBOutlet weak var bitcoinLabel: UILabel!
+    
+    let coinManager = CoinManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        currencyPicker.dataSource = self
     }
 
 
 }
 
+extension ViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManager.currencyArray.count
+    }
+    
+    
+}
