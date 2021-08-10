c9af3165a257c9b63a4e8ee217e9918ff57c7ae9
Mel <3mel.dev@gmail.com>
2021-08-10 12:21:54 +0900

    기본 UI구성 및 CoinManager model 생성

--- Bitcoin/Bitcoin/Model/BitcoinModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  BitcoinModel.swift
-//  Bitcoin
-//
-//  Created by samuel Jeong on 2021/08/08.
-//
-
-import Foundation
---
+++ Bitcoin/Bitcoin/Model/CoinManager.swift
@@ -0,0 +1,17 @@
+//
+//  CoinManager.swift
+//  Bitcoin
+//
+//  Created by samuel Jeong on 2021/08/10.
+//
+
+import Foundation
+
+struct CoinManager {
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
+    let apiKey = "401b3ad481276de0f16f0f138ce977b6"
+    
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    
+    
+}
