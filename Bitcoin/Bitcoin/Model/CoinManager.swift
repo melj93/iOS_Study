5557049b42d1b9ad4cbeee252475586f1764d439
Mel <3mel.dev@gmail.com>
2021-08-10 15:13:10 +0900

    getCoinPrice() 함수 생성.
    네트워킹 세팅 틀만 맞추기.

--- Bitcoin/Bitcoin/Model/CoinManager.swift
+++ Bitcoin/Bitcoin/Model/CoinManager.swift
@@ -9,9 +9,27 @@ import Foundation
 
 struct CoinManager {
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "401b3ad481276de0f16f0f138ce977b6"
+    let apiKey = "CB3FBE64-9E17-4CC0-AAF4-66599FF94BB2"
     
-    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    let currencyArray = ["KRW","AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD"]
     
+    func getCoinPrice(for currency: String) {
+        let urlString = "\(baseURL)/\(currency)?apikey=\(apiKey)"
+        print(urlString)
+        
+        if let url = URL(string: urlString){
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { data, response, error in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                if let safeData = data {
+                    print(safeData)
+                }
+            }
+            task.resume()
+        }
+    }
     
 }
