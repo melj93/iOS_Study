d3f6a9d13faba797e39b214a997b2dcd5b21640b
Mel <3mel.dev@gmail.com>
2021-08-12 14:43:32 +0900

    FlashChat WelcomeVC storyboard design

---
+++ FlashChat-mel/FlashChat-mel/WelcomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlashChat-mel
+//
+//  Created by samuel Jeong on 2021/08/12.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
