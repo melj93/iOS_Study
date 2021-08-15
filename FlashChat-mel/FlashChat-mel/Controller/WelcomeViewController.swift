a5f4cad39be5edbd0592d99d7cf34f7e329ccb07
Mel <3mel.dev@gmail.com>
2021-08-15 22:42:47 +0900

    WelcomeVC title animation Timer set

--- FlashChat-mel/FlashChat-mel/Controller/WelcomeViewController.swift
+++ FlashChat-mel/FlashChat-mel/Controller/WelcomeViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
-
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        var charIndex = 0.0
+        titleLabel.text = ""
+        let title = "⚡️FlashChat"
+        for letter in title {
+            print(0.1 * charIndex)
+            print(letter)
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
+                self.titleLabel.text?.append(letter)
+            }
+            charIndex += 1
+        }
     }
-
-
+    
+    @IBAction func registerPressed(_ sender: Any) {
+    }
+    @IBAction func logInPressed(_ sender: Any) {
+        
+    }
 }
 
