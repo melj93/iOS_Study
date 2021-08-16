453eb084841a1416df49ad3a0cbe1bcdd8ecbfe4
Mel <3mel.dev@gmail.com>
2021-08-16 18:19:45 +0900

    Udemy for문 문법공부

---
+++ swift grammar/forLoopPractice.playground/Contents.swift
@@ -0,0 +1,20 @@
+let friendsArray: Array = ["Hanna", "Gangsan", "Bumoon"]
+let friendsSet: Set = ["3Hanna", "2Gangsan", "1Bumoon"]
+let word: String = "pneumonoultramicroscopicsilicovolcanoconiosis"
+let halfRangeOfNumber = 1...4
+
+//for name in friendsArray { // array
+//    print(name)
+//}
+//print("---")
+//for name in friendsSet { // set
+//    print(name)
+//}
+//
+//for letter in word { // String
+//    print(letter)
+//}
+
+for _ in halfRangeOfNumber { // _ underScore as no constant
+    print("Loop is running")
+}
