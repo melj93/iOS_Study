105d800452d3f9609a69bd05ded9b5f913bc9641
Mel <3mel.dev@gmail.com>
2021-08-28 20:00:14 +0900

    LoginVC 로그인 기능 활성화 with Firebase Authentification
    +storyboard password secure text

--- FlashChat-mel/FlashChat-mel/Controller/LoginViewController.swift
+++ FlashChat-mel/FlashChat-mel/Controller/LoginViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class LogInViewContoller: UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
@@ -14,6 +15,20 @@ class LogInViewContoller: UIViewController {
     
     
     @IBAction func logInPressed(_ sender: UIButton) {
-       
+        if let email = emailTextField.text, let password = passworldTextField.text{
+            Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+                if let e = error{
+                    //Errror 발생시 안내 팝업
+                    let alert = UIAlertController(title: "Error", message: e.localizedDescription, preferredStyle: UIAlertController.Style.alert)
+                    
+                    let defaultAction = UIAlertAction(title: "OK", style: .destructive, handler: nil)
+                    alert.addAction(defaultAction)
+                    
+                    self.present(alert, animated: false, completion: nil)
+                } else {
+                    self.performSegue(withIdentifier: "LogInToChat", sender: self)
+                }
+            }
+        }
     }
 }
