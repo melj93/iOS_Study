84e8b147d0fd5bdac09f2c1603bb1f71f00495e2
Mel <3mel.dev@gmail.com>
2021-09-01 00:51:22 +0900

    adding Constants file
    and making struct Constants

---
+++ FlashChat-mel/FlashChat-mel/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  FlashChat-mel
+//
+//  Created by samuel Jeong on 2021/09/01.
+//
+
+struct Constants {
+    static let registerSegue = "RegisterToChat"
+    static let loginSegue = "LogInToChat"
+}
