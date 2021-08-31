a15641d06d94eaa793147ec18c5a5a020ac05921
Mel <3mel.dev@gmail.com>
2021-09-01 01:09:52 +0900

    Constants 변경

--- FlashChat-mel/FlashChat-mel/Constants.swift
+++ FlashChat-mel/FlashChat-mel/Constants.swift
@@ -5,7 +5,23 @@
 //  Created by samuel Jeong on 2021/09/01.
 //
 
-struct Constants {
+struct K {
+    static let cellIdentifier = "ReusableCell"
+    static let cellNibName = "MessageCell"
     static let registerSegue = "RegisterToChat"
-    static let loginSegue = "LogInToChat"
+    static let loginSegue = "LoginToChat"
+    
+    struct BrandColors {
+        static let purple = "BrandPurple"
+        static let lightPurple = "BrandLightPurple"
+        static let blue = "BrandBlue"
+        static let lighBlue = "BrandLightBlue"
+    }
+    
+    struct FStore {
+        static let collectionName = "messages"
+        static let senderField = "sender"
+        static let bodyField = "body"
+        static let dateField = "date"
+    }
 }
