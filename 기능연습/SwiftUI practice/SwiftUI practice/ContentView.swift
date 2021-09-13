7f22a48076cfd4ca4e8e78402d6a7e7d72b4611c
Mel <3mel.dev@gmail.com>
2021-09-13 15:15:45 +0900

    swiftUI 기초연습

--- 기능연습/SwiftUI practice/SwiftUI practice/ContentView.swift
+++ 기능연습/SwiftUI practice/SwiftUI practice/ContentView.swift
@@ -9,8 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text("Hello")
+            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+            .font(.largeTitle)
+            .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+            .colorInvert()
     }
 }
 
