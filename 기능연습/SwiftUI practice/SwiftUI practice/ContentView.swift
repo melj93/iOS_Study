da6675a57e29e9a9140bc3c85938799c444081cc
Mel <3mel.dev@gmail.com>
2021-09-13 15:15:32 +0900

    swiftUI 기초 연습 0913
    ~ page 35

---
+++ 기능연습/SwiftUI practice/SwiftUI practice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI practice
+//
+//  Created by samuel Jeong on 2021/09/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
