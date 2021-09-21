d3eeb20eea10fad588981cd1a1819b348db23afb
Mel <3mel.dev@gmail.com>
2021-09-21 15:02:12 +0900

    SwiftUI 튜토리얼 LandMark
    Creating and Combining Views

---
+++ 기능연습/LandMarks/LandMarks/View/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  LandMarks
+//
+//  Created by samuel Jeong on 2021/09/21.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(Circle().stroke( Color.white, lineWidth: 4))
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
