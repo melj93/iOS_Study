ca9350ac2e6c161054c7299e9277699627fc1127
Mel <3mel.dev@gmail.com>
2021-09-29 14:45:41 +0900

    웰컴뷰에서 네비게이션바 숨기기,
    뷰윌 어피어 -> 트루
    뷰윌 디서피어 -> 펄스

--- FlashChat-mel/FlashChat-mel/Controller/WelcomeViewController.swift
+++ FlashChat-mel/FlashChat-mel/Controller/WelcomeViewController.swift
@@ -11,6 +11,13 @@ class WelcomeViewController: UIViewController {
     
     @IBOutlet weak var titleLabel: UILabel!
     
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.isNavigationBarHidden = true
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        navigationController?.isNavigationBarHidden = false
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
