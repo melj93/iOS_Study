3c6ebc295bce28905981f734b7c2ce86ea2a8032
Mel <melj@kakao.com>
2021-10-27 12:24:44 +0900

    만두카드 만들기.ZStack안에 이미지, 텍스트, 라운드바 만들기. 라운드바 안에 이미지와 텍스트 넣기.

--- Mandoo_Card/Mandoo_Card/ContentView.swift
+++ Mandoo_Card/Mandoo_Card/ContentView.swift
@@ -9,20 +9,55 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-            ZStack{
-                Color(red: 0.97, green: 0.84, blue: 0.58, opacity: 1.00)
-                    .edgesIgnoringSafeArea(.all)
+        ZStack{
+            Color(.systemGray)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("mandoo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width:200.0, height: 200.0)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.white, lineWidth: 5))
+                    .padding(.bottom, 10.0)
                 Text("만두")
                     .font(Font.custom("MaruBuri-Bold", size: 40))
                     .fontWeight(.bold)
-                    .foregroundColor(Color.black)
+                    .foregroundColor(Color.white)
+                Text("고양이")
+                    .foregroundColor(.white)
+                    .font(
+                        Font.custom("MaruBuri-Regular", size: 25)
+                    )
+                    .padding(.top, 2.0)
+                Divider()
                     .padding()
+                InfoView(text: "010 9201 8000", imageName: "phone.fill")
             }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct InfoView: View {
+    
+    let text: String
+    let imageName: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25)
+            .fill(Color.white)
+            .frame(height: 50)
+            .overlay(HStack {
+                Image(systemName: imageName)
+                    .foregroundColor(.green)
+                Text(text)
+            })
+            .padding(.all)
     }
 }
