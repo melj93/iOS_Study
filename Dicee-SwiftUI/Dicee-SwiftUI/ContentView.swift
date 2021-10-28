f8fc536a8f9fe8d4588b4998ab8c16730462c9ff
Mel <3mel.dev@gmail.com>
2021-10-28 14:31:46 +0900

    background image 적용

--- Dicee-SwiftUI/Dicee-SwiftUI/ContentView.swift
+++ Dicee-SwiftUI/Dicee-SwiftUI/ContentView.swift
@@ -9,8 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            
+            Text("Hello, world!")
+                .padding()
+        }
+        
     }
 }
 
