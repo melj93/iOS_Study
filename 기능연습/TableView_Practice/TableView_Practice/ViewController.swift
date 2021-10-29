77783278a1b4621987a4046643e3a9e656869185
Mel <melj@kakao.com>
2021-10-29 16:23:26 +0900

    Using contentContfiguration in Cell

--- 기능연습/TableView_Practice/TableView_Practice/ViewController.swift
+++ 기능연습/TableView_Practice/TableView_Practice/ViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let ara = ["a", "b", "c"]
+    let star = UIImage(systemName: "star")
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
 
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return ara.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell :UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        
+        
+        content.text = ara[indexPath.row]
+        content.image = star
+        cell.contentConfiguration = content
+        return cell
+    }
+    
+    
+    
+}
