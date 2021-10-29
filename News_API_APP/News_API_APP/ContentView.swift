7addac9bb6b5dc438770c7e77c74bf798e76f546
Mel <melj@kakao.com>
2021-10-29 14:22:35 +0900

    New Project News_API app

---
+++ News_API_APP/News_API_APP/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  News_API_APP
+//
+//  Created by samuel Jeong on 2021/10/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        List{
+            Text("Hello, world!")
+            Text("Goodbye, world!")
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
